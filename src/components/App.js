1ff622f8379c4145fb82f32df11ec5d7b932b93e
charles <cyphersmae@gmail.com>
2025-07-17 16:41:41 +0300

    complete imports and exports lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 function App() {
   return (
     <div>
@@ -8,6 +10,5 @@ function App() {
       <About />
     </div>
   );
-}
-
+} 
 export default App;
